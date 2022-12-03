3db75d75310f98acb1051ed1a5415a236d7bef32
Jonas Gierer <jonas@gierer.xyz>
2022-12-03 01:52:47 +0100

    Update template

--- day-xx/src/main.rs
+++ day-xx/src/main.rs
@@ -3,6 +3,10 @@ use utils::read_input_file;
 fn main() {
     let input = read_input_file();
 
-    let result = input;
-    println!("Part one result: {result}");
+    for line in input.lines() {
+        dbg!(line);
+    }
+
+    println!("Part one result: {:?}", todo!());
+    println!("Part two result: {:?}", todo!());
 }
